0e64dfadad34c9351b2043c9039e600f8ddf802d
umpu <carol.ledgers0h@icloud.com>
2024-03-04 19:00:50 +0500

    Add external AttributedString

--- Fathers-Help-2/ContentView.swift
+++ Fathers-Help-2/ContentView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State var containerSize: Double
     
+    /// Подозреваю что верное решение будет потом в этой конструкции
+    var attributedString: AttributedString {
+        var attributedString = AttributedString("Марафон")
+        attributedString.foregroundColor = .gray
+        attributedString += " " // Неразрывный пробел через option + space
+        return attributedString
+    }
+    
     var body: some View {
         VStack {
             VStack(alignment: .leading) {
-                Text("Марафон ").foregroundStyle(.gray) + Text("по SwiftUI")
-                //Text("Марафон по SwiftUI")
+                Text("\(attributedString)по SwiftUI")
                 
                 Text("«Отцовский Пинок»")
                     .bold()
